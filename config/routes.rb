d232b7d171ca1a9f6eee403a39c3ca43f990dc5c
nahumst <nahum.st@icloud.com>
2016-06-24 19:31:17 -0500

    Blog Fase Beta 0.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :posts
+  resources :comments
+  resources :comments
+  resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
